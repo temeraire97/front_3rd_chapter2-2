968dce7dc5fcf54037c55acb53132a9d5c0bdd3a
HyunsooYun97 <hyensooyoon@primeblock.co>
2024-10-22 01:31:35 +0900

    UPDATE: useProduct 완성

--- src/refactoring/hooks/useProduct.ts
+++ src/refactoring/hooks/useProduct.ts
@@ -1,6 +1,22 @@
 import { useState } from 'react';
 import { Product } from '../../types.ts';
 
-export const useProducts = (initialProducts: Product[]) => {
-  return { products: [], updateProduct: () => undefined, addProduct: () => undefined };
+interface ProductHook {
+  products: Product[];
+  updateProduct: (product: Product) => void;
+  addProduct: (product: Product) => void;
+}
+
+export const useProducts = (initialProducts: Product[]): ProductHook => {
+  const [products, setProducts] = useState(initialProducts);
+
+  const updateProduct = (updateProduct: Product) => {
+    setProducts((prevProd) => prevProd.map((product) => (product.id === updateProduct.id ? updateProduct : product)));
+  };
+
+  const addProduct = (newProduct: Product) => {
+    setProducts((prevProd) => [...prevProd, newProduct]);
+  };
+
+  return { products, updateProduct, addProduct };
 };
