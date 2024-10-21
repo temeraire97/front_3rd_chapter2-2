31c8e8272df0860d910932bf1a5b7a6617e271a2
HyunsooYun97 <hyensooyoon@primeblock.co>
2024-10-22 01:24:54 +0900

    UPDATE: useCoupon 완성

--- src/refactoring/hooks/useCoupon.ts
+++ src/refactoring/hooks/useCoupon.ts
@@ -2,5 +2,11 @@ import { Coupon } from '../../types.ts';
 import { useState } from 'react';
 
 export const useCoupons = (initialCoupons: Coupon[]) => {
-  return { coupons: [], addCoupon: () => undefined };
+  const [coupons, setCoupons] = useState(initialCoupons);
+
+  const addCoupon = (coupon: Coupon) => {
+    setCoupons([...coupons, coupon]);
+  };
+
+  return { coupons, addCoupon };
 };
